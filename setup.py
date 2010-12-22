0dc0482b553ff5a8b1306346271863880191e60d
Jürgen Kartnaller <juergen@kartnaller.at>
2010-12-22 09:14:11 +0100

    increase package version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup (
         + '\n\n' +
         read('CHANGES.txt')
         ),
-    version='0.0.2',
+    version='0.0.3',
     author = "Lovely Systems",
     author_email = "office@lovelysystems.com",
     license = "ZPL 2.1",
